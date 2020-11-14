ed097917aae6339d0c15fa32ee76c68ee4c82122
Vishal Nagda <vishalnagda1@gmail.com>
2020-11-14 17:13:34 +0530

    reading text from command line args

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pyttsx3
+import argparse
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+
+# Adding optional argument
+parser.add_argument("-t", "--text", help="Command line input of text")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+text_to_read = "Hi there! I'll read text for you."
+if args.text:
+    text_to_read = args.text
 
 # initialize the reading engine
 reader = pyttsx3.init()
 
-# Sample text to read
-reader.say("Hi there! I'll read text for you.")
+# read the given text
+reader.say(text_to_read)
 
 # Execution of reading process
 reader.runAndWait()
